544cb4e62de3fc0016d5166c0431a6626f0ba57e
Nina <n.mancovic@gmail.com>
2019-08-10 00:11:43 +0200

    readme added

--- Test-Humanity.py
+++ Test-Humanity.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import random
 
 driver = webdriver.Chrome(executable_path="C:\chromedriver.exe")
 
@@ -23,9 +24,14 @@ driver.find_element_by_xpath("//*[@id=\"act_primary\"]").click()
 
 # Entering information for the employee
 time.sleep(5)
-driver.find_element_by_id("_asf1").send_keys("Maja") #Fill in name
-driver.find_element_by_id("_asl1").send_keys("Pcelica") #Fill in Last name
-driver.find_element_by_id("_ase1").send_keys("p.maja@gmail.com") #Fill in E-mail
+rand = str(random.randint(0,500))
+employees_name= "Maja" + rand
+employees_lastname= "Pcelica" + rand
+employees_email = "p.maja" + rand + "@gmail"
+
+driver.find_element_by_id("_asf1").send_keys(employees_name) #Fill in name
+driver.find_element_by_id("_asl1").send_keys(employees_lastname) #Fill in Last name
+driver.find_element_by_id("_ase1").send_keys(employees_email) #Fill in E-mail
 
 # Confirming entry => Save Employees
 driver.find_element_by_id("_as_save_multiple").click()
---
+++ readme.md
@@ -0,0 +1,24 @@
+#Humanity login and add employee test
+
+
+## Installation instruction for Windows 10
+
+1. Download and install [python 3.7](https://www.python.org/downloads/).
+
+2. Download [chromedriver.exe](https://chromedriver.chromium.org/downloads).
+
+3. Move chromedriver.exe to "C:\".
+
+4. Install selenium webdriver
+
+```bash
+pip install selenium
+```
+
+## Usage
+
+1. Run test.
+
+```bash
+python Test-Humanity.py
+```
